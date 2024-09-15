c9c4dd1162c3f711e0edce327fadfa8062e10aa1
subha4256 <43105434+subha4256@users.noreply.github.com>
2024-09-15 10:10:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+def hello():
+    return 'Hello, World!'
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
