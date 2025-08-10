a740a91db131cdbac692cdd939c25f17dbc69667
subha4256 <43105434+subha4256@users.noreply.github.com>
2025-08-10 18:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI Amplo!"}
+    return {"message": "Hello from FastAPI Amplo!ggggg"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
